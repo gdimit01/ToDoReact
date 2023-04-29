6b0c776153c8658fe19fbfd9301a865d17474538
Georgi Dimitrov <gd353@kent.ac.uk>
2023-04-29 13:55:37 +0100

    setNewItem function enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  
+
   return (
     <>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="Item">New Item</label>
+          <input value ={newItem} onChange={e => setNewItem(e.target.value)} type = "text" id="Item" />
           <input type="text" id="Item" />
         </div>
         <button className="btn">Add</button>
